826c76e2c999df1588df528b2eec564f29d06bfd
Ronid1 <ronidiwan@gmail.com>
2022-12-13 18:47:52 +0200

    user schema

--- controllers/users-controllor.js
+++ controllers/users-controllor.js
@@ -1,43 +1,59 @@
 const uuid = require("uuid").v4;
 const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
+const User = require("../models/user");
 
-let DUMMY_USERS = [
-  {
-    id: "u1",
-    name: "user one",
-    email: "user@email.com",
-    password: "password1",
-  },
-];
+async function getUsers(req, res, next) {
+  let users;
+  try {
+    users = await User.find({}, "-password");
+  } catch {
+    return next(new HttpError("Something went wrong", 500));
+  }
 
-function getUsers(req, res, next) {
-  res.json({ users: DUMMY_USERS });
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 }
 
-function createUser(req, res, next) {
+async function createUser(req, res, next) {
   const errors = validationResult(req);
   if (!errors.isEmpty())
     return next(new HttpError("Invalid inputs passed", 422));
 
-  const { name, email, password } = req.body;
-  if (DUMMY_USERS.find((user) => user.email === email))
-    return next(new HttpError("User already exists", 422));
+  const { name, email, password, places } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    return next(new HttpError("Something went wrong", 500));
+  }
 
-  const newUser = {
-    id: uuid(),
+  if (existingUser) return next(new HttpError("User already exists", 422));
+
+  const newUser = new User({
     name,
     email,
     password,
-  };
-  DUMMY_USERS.push(newUser);
+    places,
+  });
 
-  res.status(201).json({ user: newUser });
+  try {
+    await newUser.save();
+  } catch (err) {
+    return next(new HttpError("Something went wrong", 500));
+  }
+
+  res.status(201).json({ user: newUser.toObject({ getters: true }) });
 }
 
-function loginUser(req, res, next) {
+async function loginUser(req, res, next) {
   const { email, password } = req.body;
-  const user = DUMMY_USERS.find((user) => user.email === email);
+
+  let user;
+  try {
+    user = await User.findOne({ email: email });
+  } catch (err) {
+    return next(new HttpError("Something went wrong", 500));
+  }
   if (!user || user.password !== password)
     return next(new HttpError("wrong credentials", 401));
 
