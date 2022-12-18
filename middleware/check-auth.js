8d9f7a5cfd27a339029f0af3fad7be58aa793575
Ronid1 <ronidiwan@gmail.com>
2022-12-18 15:42:06 +0200

    fix jwt

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -3,15 +3,16 @@ const dotenv = require("dotenv").config();
 const HttpError = require("../models/http-error");
 
 function checkAuth(req, res, next) {
+  if (req.method === "OPTIONS") return next();
   try {
-    const token = req.heaers.authorization.split(" ")[1];
+    const token = req.headers.authorization.split(" ")[1];
     if (!token) return next(new HttpError("Auth failed", 401));
 
     const decodedToken = jwt.verify(token, process.env.TOKEN_CODE);
     req.userData = { userId: decodedToken.userId };
     next();
   } catch (error) {
-    return next(new HttpError());
+    return next(new HttpError("Unauthorized", 500));
   }
 }
 
